14260bfb3f69ad8c11ade3a50cdf62f1d6cb2a75
LaetitiaMagniez <lmagniez03@gmail.com>
2025-05-29 13:38:35 +0200

    update map

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'core/widgets/auth_gate.dart';
 import 'firebase_options.dart';
 import 'core/providers/theme_provider.dart';
-import 'dart:io';
 
 void main() {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
-
-    try {
-      // Charger le fichier .env.android
-      await dotenv.load(fileName: '.env.android');
-    } catch (e, stackTrace) {
-      print('Erreur lors du chargement du fichier .env.android : $e');
-      print('Trace d\'appel : $stackTrace');
-      // Vous pouvez également afficher un message d'erreur à l'utilisateur ici
-      return;
-    }
 
     try {
       await Firebase.initializeApp(
@@ -28,7 +16,6 @@ void main() {
       );
     } catch (e) {
       print('Erreur d\'initialisation Firebase : $e');
-      // Vous pouvez également afficher un message d'erreur à l'utilisateur ici
       return;
     }
 
