c44fd0583dce4f2e931dc9c126a5da012433a864
LaetitiaMagniez <lmagniez03@gmail.com>
2025-06-17 10:28:13 +0200

    mise à jour deploy

---
+++ generate_dummy_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "// GENERATED DUMMY FILE FOR WEB BUILD" > lib/env.dart
+echo "class Env {" >> lib/env.dart
+echo "  static const String DUMMY = 'dummy';" >> lib/env.dart
+echo "}" >> lib/env.dart
