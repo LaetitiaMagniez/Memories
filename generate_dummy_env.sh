aa803f963bfdb1f569d33935cf817a051f260def
LaetitiaMagniez <lmagniez03@gmail.com>
2025-06-17 11:05:48 +0200

    mise à jour index et deploy

--- generate_dummy_env.sh
+++ generate_dummy_env.sh
@@ -2,5 +2,5 @@
 
 echo "// GENERATED DUMMY FILE FOR WEB BUILD" > lib/env.dart
 echo "class Env {" >> lib/env.dart
-echo "  static const String DUMMY = 'dummy';" >> lib/env.dart
+echo "  static const String NINJA_API_KEY = 'DUMMY_WEB_DO_NOT_USE';" >> lib/env.dart
 echo "}" >> lib/env.dart
--- lib/features/map/services/map_service_web.dart
+++ lib/features/map/services/map_service_web.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:js/js.dart';
 import 'dart:js_interop';
+import '../../../env.dart' if (dart.library.js) '../../../env_dummy.dart';
 
 @JS('env')
 external _EnvJS? get env;
@@ -14,5 +16,9 @@ extension _EnvJSKeys on _EnvJS {
 }
 
 String get ninjaApiKey {
-  return env?.NINJA_API_KEY?.toDart ?? '';
+  if (kIsWeb) {
+    return env?.NINJA_API_KEY?.toDart ?? '';
+  } else {
+    return Env.NINJA_API_KEY;
+  }
 }
